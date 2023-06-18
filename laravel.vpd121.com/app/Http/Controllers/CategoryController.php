19ca8bf8394a2e5ef6b0c5c276f19e8deb491853
novakvova <novakvova@gmail>
2023-06-18 17:07:32 +0300

    create category

--- laravel.vpd121.com/app/Http/Controllers/CategoryController.php
+++ laravel.vpd121.com/app/Http/Controllers/CategoryController.php
@@ -19,4 +19,38 @@ class CategoryController extends Controller
         return response()->json($list, 200,
         ["Content-Type"=>"application/json;charset=UTF-8", "Charset" => "utf-8"], JSON_UNESCAPED_UNICODE);
     }
+
+    /**
+     * @OA\Post(
+     *     tags={"Category"},
+     *     path="/api/category",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={},
+     *                 @OA\Property(
+     *                     property="image",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="description",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Add Category.")
+     * )
+     */
+    public function create(Request $request) {
+        $input = $request->all();
+        $category = Category::create($input);
+        return response()->json($category, 201,
+            ["Content-Type"=>"application/json;charset=UTF-8", "Charset" => "utf-8"], JSON_UNESCAPED_UNICODE);
+    }
 }
