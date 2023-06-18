6d3ddf9ddad825d427ddd46a25256748d7a24fd8
novakvova <novakvova@gmail>
2023-06-18 16:19:44 +0300

    Begin working swagger

--- laravel.vpd121.com/app/Http/Controllers/CategoryController.php
+++ laravel.vpd121.com/app/Http/Controllers/CategoryController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     tags={"Category"},
+     *     path="/api/category",
+     *     @OA\Response(response="200", description="List Categories.")
+     * )
+     */
     public function index() {
         $list = Category::all();
         return response()->json($list, 200,
